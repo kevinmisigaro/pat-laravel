81a37077334ea10afe90fdee7c6ab873c6d9760e
Kevin Misigaro <kevin@databalk.nu>
2024-08-17 19:46:14 +0200

    make coauthor nullable

--- database/migrations/2024_08_17_172245_add_coauthor.php
+++ database/migrations/2024_08_17_172245_add_coauthor.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('abstracts', function (Blueprint $table) {
-            $table->string('coauthor');
+            $table->string('coauthor')->nullable();
         });
     }
 
