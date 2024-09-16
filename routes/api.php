366f3a500cafb78b852cc6b059679585970d5d05
Kevin Misigaro <kevin@databalk.nu>
2024-09-16 09:27:01 +0200

    installing api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\ControlController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::post("/resetpassword", [ControlController::class, 'resetPassword'])->name('resetpassword');
